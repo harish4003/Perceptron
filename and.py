ee38cbe6ca4865b782411251f14db8a3caa631a4
harish <harishkumar604@yahoo.com>
2022-02-24 15:31:58 +0530

    and.py is updated

--- and.py
+++ and.py
@@ -1,28 +1,34 @@
+from fileinput import filename
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
-AND = {
+def main(data, eta, epochs, filename, plotname):
+
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+    save_model(model, filename="and.model")
+    save_plot(df, "and.png", model)
+
+if __name__=="__main__":  # entry point
+    AND = {
     "x1": [0,0,1,1],
     "x2": [0,1,0,1],
     "y": [0,0,0,1],
-}
+    }
 
-df = pd.DataFrame(AND)
+    df = pd.DataFrame(AND)
 
-df
+    df
 
 
-X,y = prepare_data(df)
+    X,y = prepare_data(df)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
-
-save_model(model, filename="and.model")
-save_plot(df, "and.png", model)
+    main(data = AND,eta = ETA, epochs = EPOCHS, filename= "and.model", plotname = "and.png")
